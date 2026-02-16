3d5478899b1ffb9188d7f0dde4a09a37ed10629e
nithinsettibathula <nithinsece58@gmail.com>
2026-02-16 17:57:48 +0530

    Fix: Removed invalid semicolons and formatted HCL

--- main.tf
+++ main.tf
@@ -3,19 +3,40 @@ provider "aws" {
 }
 
 resource "aws_instance" "strapi_server" {
-  # Standard Amazon Linux 2 AMI (Highly Stable)
   ami           = "ami-0c101f26f147fa7fd" 
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
-  tags = { Name = "Debug-Server-Nithin" }
+  tags = {
+    Name = "Debug-Server-Nithin"
+  }
 }
 
 resource "aws_security_group" "strapi_sg" {
-  name = "strapi_sg_debug"
-  ingress { from_port = 22; to_port = 22; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
-  ingress { from_port = 80; to_port = 80; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
-  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
+  name = "strapi_sg_debug_v3"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-output "public_ip" { value = aws_instance.strapi_server.public_ip }
+output "public_ip" {
+  value = aws_instance.strapi_server.public_ip
+}
